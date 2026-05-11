f64779164e750969e90c272bcb1baf4b325910b0
S0iRu <108854997+S0iRu@users.noreply.github.com>
2026-05-11 18:14:57 +0900

    test60

--- components/providers/FriendsProvider.tsx
+++ components/providers/FriendsProvider.tsx
@@ -1070,7 +1070,7 @@ export const FriendsProvider = ({ children }: { children: React.ReactNode }) => 
             if (isAuthenticatedRef.current) {
                 fetchFriendsRef.current();
             }
-        }, 1_000);
+        }, 30_000);
         // }, 300_000);
 
         return () => {
